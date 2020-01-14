b143ff4064212a12e9238c7e25628b08a94210fe
Quist0n <37319086+Quist0n@users.noreply.github.com>
2020-01-14 12:40:54 -0500

    Minor updates....

--- download.sh
+++ download.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-VERSION=0.5
+VERSION=0.6
 MODE=$1
-
 
 function normal_mode()
 {
@@ -11,34 +10,27 @@ function normal_mode()
 
 function playlist()
 {
-		youtube-dl -vv --external-downloader axel --external-downloader-args "-n $2 -a" "$3" -o "%(autonumber)s - %(title)s.%(ext)s";
+	youtube-dl -vv --external-downloader axel --external-downloader-args "-n $2 -a" "$3" -o "%(autonumber)s - %(title)s.%(ext)s";
 };
 
-output_det=0;
-function generic()
+function single-con()
 {
-	if [ $output_det == 0 ];
-	then
-		youtube-dl -vv "$2" -o "$3";
-	else
-		youtube-dl -vv "$2";
-	fi
+	youtube-dl -vv "$2" -o "$3";
 };
 
 
 if [ "$MODE" == "--normal" ] || [ "$MODE" == "-n" ]
 	then
 	normal_mode "$@";
-elif [ "$MODE" == "--generic" ] || [ "$MODE" == "-g" ];
+
+elif [ "$MODE" == "--single-con" ] || [ "$MODE" == "-si" ];
 	then
-	generic "$@";
+	single-con "$@";
+
 elif [ "$MODE" == "--playlist" ] || [ "$MODE" == "-p" ];
 	then 
 	playlist "$@";
-elif [ -z "$MODE" ];
-	then 
-	output_det=1;
-	generic "$@";
+
 elif [ "$MODE" == "--help" ] || [ "$MODE" == "-h" ];
 	then
 	echo "Usage : $0 [MODE] [URL]";
@@ -48,11 +40,8 @@ elif [ "$MODE" == "--help" ] || [ "$MODE" == "-h" ];
 	echo "-p | --playlist				Downloads a youtube playlist or other playlist supported by youtube-dl using axel to current working directory
 	using N number of connections";
 	echo "(e.g $0 -p [Number of connections] [URL])";
-	echo "-g | --generic 				Downloads the file and writes it to [FILE], for use where multiple connections cannot be used";
-	echo "(e.g $0 -g [URL] [Renamed file name])";
-	echo "-g | --generic also can be invoked without [Remaned file name] and will default to the file name on the server" 
-
-else
-	echo "Usage : $0 [MODE] [URL]";
-	echo "Use $0 [-h|--help] for options";
+	echo "-sc | --single-con 				Downloads the file and writes it to [FILE], for use where multiple connections cannot be used";
+	echo "(e.g $0 -sc [URL] [Renamed file name])";
+else 
+	echo "Usage: $0 [MODE] [URL]";
 fi 
