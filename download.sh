86015a4baae8f767a343f23789aecde2f2a52cc0
Quist0n <37319086+Quist0n@users.noreply.github.com>
2020-01-18 17:25:30 -0500

    Added --hls-prefer-native back to single-con, since it WAS the thing that fixed the bug. Also added note that urls are to be surrounded in quotes, otherwise with certain urls, the youtube-dl can't parse it, as well as showing that invoking script with no options can be used to run youtube-dl and pass option in natively. Though at that point, you should just run youtube-dl natively. Passing youtube-dl options into the script is untest however, not recommended,but totally possible.

--- download.sh
+++ download.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VERSION=0.6.2
+VERSION=0.6.4
 MODE=$1
 
 function normal_mode()
@@ -15,7 +15,7 @@ function playlist()
 
 function single-con()
 {
-	youtube-dl -vv "$2" -o "$3";
+	youtube-dl -vv --hls-prefer-native "$2" -o "$3";
 };
 
 function default_job() 
@@ -39,7 +39,7 @@ elif [ "$MODE" == "--help" ] || [ "$MODE" == "-h" ];
 	then
 	echo "Usage : $0 [MODE] [URL]";
 	echo "Invoking the script without a mode set will just invoke youtube-dl with debug output enabled pointed to download the url you
-	specified.";
+	specified. This can also allow you to pass youtube-dl options in, but this is untested, but SEEMS to work";
 	echo "Modes:";
 	echo "-n | --normal					Downloads file using axel with N number of connections and writes file to [FILE]";
 	echo "(e.g $0 -n [Number of connections] [URL] [FILE])";
@@ -48,7 +48,7 @@ elif [ "$MODE" == "--help" ] || [ "$MODE" == "-h" ];
 	echo "(e.g $0 -p [Number of connections] [URL])";
 	echo "-s | --single-con 				Downloads the file and writes it to [FILE], for use where multiple connections cannot be used";
 	echo "(e.g $0 -sc [URL] [Renamed file name])";
-	echo "\n";
+	echo "";
 	echo "If you are having an error in the script with youtube-dl unable to read the url, please surround it in quotes when you invoke the
 	script.";
 else 	
