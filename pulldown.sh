a8e43f2534f01dd11ae769ff5bdecd57706caaa1
Quist0n <37319086+Quist0n@users.noreply.github.com>
2020-12-18 02:19:45 -0500

    Added audio extraction mode, made it so that you can pass arbitrary youtube-dl flags to playlist mode as well as audio mode. Made all modes add the metadata to the file downloaded if there is any. Updated help section.

--- pulldown.sh
+++ pulldown.sh
@@ -1,56 +1,79 @@
 #!/bin/bash
 
-VERSION=0.6.5.1
+VERSION=0.6.5.2
 MODE=$1
+NAME="pulldown.sh"
 
 function normal_mode()
 {
- 	youtube-dl -v  --external-downloader axel "$1" -o "$2";
+ 	youtube-dl -v --add-metadata --external-downloader axel "$1" -o "$2";
 };
 
 function playlist()
 {
-	youtube-dl -v "$1" -o "%(autonumber)s-%(title)s.%(ext)s";
+	youtube-dl -v "${@:2}" --add-metadata "$1" -o "%(autonumber)s-%(title)s.%(ext)s";
+};
+
+function audio_job()
+{
+        youtube-dl -v -x "${@:2}" --add-metadata "$1" -o "%(autonumber)s-%(title)s.%(ext)s";
 };
 
 function native_hls()
 {
-	youtube-dl -v --hls-prefer-native "$1" -o "$2";
+	youtube-dl -v --add-matadata --hls-prefer-native  "$1" -o "$2";
 };
 
 function default_job() 
 {
 	youtube-dl -v "$@";
-}
+};
+
 
 if [ "$MODE" == "--normal" ] || [ "$MODE" == "-n" ]
 	then
 	normal_mode "$2" "$3";
 
-elif [ "$MODE" == "--native-hls" ] || [ "$MODE" == "-s" ] || [ "$MODE" == "-hls" ];
+elif [ "$MODE" == "--native-hls" ] || [ "$MODE" == "-s" ] || [ "$MODE" == "--hls" ];
 	then
 	native_hls "$2" "$3";
 
 elif [ "$MODE" == "--playlist" ] || [ "$MODE" == "-p" ];
 	then 
-	playlist "$2";
-
+	playlist "$2" "${@:3}";
+elif [ "$MODE" == "--audio" ] || [ "$MODE" == "-a" ];
+    then
+    audio_job "$2" "${@:3}";
 elif [ "$MODE" == "--help" ] || [ "$MODE" == "-h" ];
 	then
-	echo "Usage : $0 [MODE] URL [FILE]";
+    echo "PullDown $VERSION";
+	echo "Usage : $NAME [MODE] URL [FILE]";
 	echo "Invoking the script without a mode set will just invoke youtube-dl with debug output enabled pointed to download the url you
-	specified. This can also allow you to pass youtube-dl options in, but this is untested, but SEEMS to work";
+	specified. This can also allow you to pass youtube-dl
+    options in, but this is untested, but SEEMS to work.";
+
 	echo "Modes:";
-	echo "-n | --normal					Downloads file using axel directed to to [FILE] or the default file name";
-	echo "(e.g $0 -n  [URL] [FILE])";
-	echo "-p | --playlist				Downloads a youtube playlist or other playlist supported by youtube-dl to current working directory";
-	echo "(e.g $0 -p [URL])";
-	echo "-s | -hls |--native-hls 				Downloads the file and writes it to [FILE] or the default file name, for use where the axel
+	echo "-n | --normal					Downloads file using axel directed to [FILE] or the default file name";
+	echo "(e.g $NAME -n  [URL] [FILE])";
+
+	echo "-p | --playlist				Downloads a youtube playlist or other playlist supported by youtube-dl to current working directory
+    with a output format set to number all downloads. You can pass youtube-dl options to this mode but don't use -o in your options as it would
+    conflict with the -o already used";
+	echo "(e.g $NAME -p URL [OPTIONS])";
+
+	echo "-a | --audio				Downloads audio to current working directory
+    with a output format set to number all downloads. You can pass youtube-dl options to this mode but don't use -o in your options as it would
+    conflict with the -o already used. This one is almost the
+    same as the playlist mode but simply prevents video from
+    being downloaded.";
+    echo "(e.g $NAME -a URL [OPTIONS])"
+
+	echo "-s | --hls |--native-hls 				Downloads the file and writes it to [FILE] or the default file name, for use where the axel
     downloader cannot be used";
-	echo "(e.g $0 -sc [URL] [Renamed file name])";
-	echo "";
+	echo "(e.g $NAME -s URL [FILE])";
+
 	echo "If you are having an error in the script with youtube-dl unable to read the url, please surround it in quotes when you invoke the
 	script.";
 else 	
-	default_job "$@";
+	default_job "${@:2}";
 fi 
