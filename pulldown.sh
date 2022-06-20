6b76906ae77f8aae893af708bad41a14b5fca324
Quist0n <37319086+Quist0n@users.noreply.github.com>
2022-06-19 22:17:55 -0500

    Remove useless  variable

--- pulldown.sh
+++ pulldown.sh
@@ -1,38 +1,37 @@
 #!/bin/bash
 YTDL="/usr/bin/youtube-dl"
-VERSION=0.8.3
+VERSION=0.8.4
 NAME='pulldown.sh'
 OUTPUT_FORMAT_NORMAL='%(title)s.%(ext)s'
 OUTPUT_FORMAT_PLAYLIST='%(playlist_autonumber)s-%(title)s.%(ext)s'
 #Some default flags to pass no matter what
 ARGS="-v --restrict-filenames"
-URLS=""
 YTDL_ARGS=""
 
 usage()
 {
         cat <<- _end_help
-Usage : $NAME [MODE] URL [OPTIONS]
+Usage : $NAME [MODE] [OPTIONS]
 Modes:
         -n | --normal
         Download using a standard file naming template template '$OUTPUT_FORMAT_NORMAL'
-        (e.g $NAME -n  [URL] [OPTIONS])
+        (e.g $NAME -n [OPTIONS])
 
         -p | --playlist
         Download playlists using the output template '$OUTPUT_FORMAT_PLAYLIST'
-        (e.g $NAME -p  [URL] [OPTIONS])
+        (e.g $NAME -p [OPTIONS])
 
         -a | --audio
         Downloads audio only.
-        (e.g $NAME -a URL [OPTIONS])
+        (e.g $NAME -a [OPTIONS])
 
         -s | --hls | --native-hls
         Downloads the file with the youtube-dl native HLS downloader
-        (e.g $NAME -s URL [OPTIONS])
+        (e.g $NAME -s [OPTIONS])
 
         -m | --metadata
         Add metadata to the file
-        (e.g $NAME -m URL [OPTIONS])
+        (e.g $NAME -m [OPTIONS])
 
         If you are having an error in the script with youtube-dl unable to read the url, please surround it in quotes when you invoke the script.
 _end_help
@@ -51,16 +50,10 @@ while [ "$1" ]; do
                         ARGS="${ARGS} --add-metadata " ;;
                 --help | -h)
                          usage; exit ;;
-                http*://*)
-                        URLS="${URLS} $1 " ;;
                 *)
                         YTDL_ARGS="${YTDL_ARGS} $1 " ;;
         esac
         shift
 done
 
-if [ -z "$URLS" ]; then
-        printf "Please specify a url to act upon (E.g https://example.com/video.mp4)\n"; exit 1
-else
-        $YTDL $ARGS $URLS $YTDL_ARGS;
-fi
+$YTDL $ARGS $YTDL_ARGS;
